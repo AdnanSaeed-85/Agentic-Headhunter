8687b721a47b655be9716fbddd1734f64b71e3dd
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-01-18 23:49:58 +0500

    simplest chatbot developed

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-import undetected_chromedriver as uc
-import time
+from langgraph.graph import START, END, StateGraph
+from dotenv import load_dotenv
+from typing import List, TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage, AIMessage
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from CONFIG import GROQ_MODEL, OPENAI_MODEL, TEMPERATURE
+from langgraph.graph.message import add_messages
 
-def run():
-    print("Launching Stealth Browser...")
-    
-    # This launches a REAL Chrome instance that bypasses bot detection
-    driver = uc.Chrome(headless=False, use_subprocess=True)
-    
-    # Go to Indeed Dubai
-    url = "https://ae.indeed.com/jobs?q=AI+Engineer&l=Dubai"
-    driver.get(url)
+load_dotenv()
+groq_llm = ChatGroq(model=GROQ_MODEL, temperature=TEMPERATURE)
+openai_llm = ChatOpenAI(model=OPENAI_MODEL, temperature=TEMPERATURE)
 
-    print("Browser open. If the checkbox appears, click it once (it should work now).")
-    
-    # Keep the browser open so you can see the result
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Closing...")
-        driver.quit()
+class state_class(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
 
-if __name__ == "__main__":
-    run()
+def chat_node(state: state_class):
+    messages = state['messages']
+    response = groq_llm.invoke(messages)
+    return {'messages': response}
+
+def main():
+    graph = StateGraph(state_class)
+    graph.add_node('chat_node', chat_node)
+    graph.add_edge(START, 'chat_node')
+    graph.add_edge('chat_node', END)
+    bot = graph.compile()
+
+    response = bot.invoke(
+        {
+            'messages': [HumanMessage(content='hi how are you?, please make response short')]
+        }
+    )
+
+    print(response['messages'][-1].content)
+
+
+if __name__ == '__main__':
+    main()
